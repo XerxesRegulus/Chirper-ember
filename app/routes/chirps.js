04ac275266aaa39331c18e68be9767d23b7b6ee5
John Paul Ricarse <johnpauliloilo@gmail.com>
2015-10-13 15:44:49 +0800

    added sessions user to new chirp

--- app/routes/chirps.js
+++ app/routes/chirps.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    return this.csrf.fetchToken();
-  },
   model: function(){
     return this.store.findAll('chirp');
   },
